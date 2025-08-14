82226bd148ff966ad8f655fb80a72ce422c60ef7
Riyaz-Gazi <riyazgazi9@gmail.com>
2025-08-14 14:57:12 +0530

    added

---
+++ connections-service/src/main/java/com/codingshuttle/linkedin/connections_service/entity/Person.java
@@ -0,0 +1,41 @@
+package com.codingshuttle.linkedin.connections_service.entity;
+
+
+import org.springframework.data.neo4j.core.schema.GeneratedValue;
+import org.springframework.data.neo4j.core.schema.Id;
+import org.springframework.data.neo4j.core.schema.Node;
+
+@Node
+public class Person {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private Long userId;
+    private String name;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
